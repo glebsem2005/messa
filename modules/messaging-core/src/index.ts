eae4e8b67ef5f4a9c902a5e4a120fc34676bfe7d
Gleb Semenov <144787082+glebsem2005@users.noreply.github.com>
2025-07-13 16:23:18 +0300

    Create index.ts

---
+++ modules/messaging-core/src/index.ts
@@ -0,0 +1,7 @@
+export * from './services/SignalProtocolService';
+export * from './services/MLSService';
+export * from './services/MessageEncryptionService';
+export * from './services/ForwardSecrecyService';
+export * from './types';
+export * from './models/Message';
+export * from './models/Conversation';
