a9a022d6fbe483c43c74f8264fbb7382a6b30d16
Gleb Semenov <144787082+glebsem2005@users.noreply.github.com>
2025-07-13 15:29:35 +0300

    Create index.ts

---
+++ modules/auth-biometry/src/index.ts
@@ -0,0 +1,6 @@
+export * from './services/BiometryService';
+export * from './services/FaceRecognitionService';
+export * from './services/IdentityService';
+export * from './types';
+export * from './hooks/useBiometryAuth';
+export * from './components/FaceCaptureModal';
