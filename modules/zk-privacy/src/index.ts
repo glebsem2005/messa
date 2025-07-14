10bd943043bc9181a5df208d51612598325dd396
Gleb Semenov <144787082+glebsem2005@users.noreply.github.com>
2025-07-14 10:07:38 +0300

    Update index.ts

--- modules/zk-privacy/src/index.ts
+++ modules/zk-privacy/src/index.ts
@@ -1,1 +1,6 @@
-
+export * from './services/ZKPrivacyService';
+export * from './services/AnonymitySetService';
+export * from './services/ProofGenerator';
+export * from './services/ProofVerifier';
+export * from './circuits';
+export * from './types';
